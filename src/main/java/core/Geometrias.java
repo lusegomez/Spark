f90f6adb4098028d67719a4c81043aec5f83e9d1
Lucas Gomez <lugomez@itba.edu.ar>
2024-07-16 00:31:50 -0300

    Validating simple quads

--- src/main/java/core/Geometrias.java
+++ src/main/java/core/Geometrias.java
@@ -3,12 +3,15 @@ package core;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
 import org.graphframes.GraphFrame;
 
 import javax.xml.crypto.Data;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -74,8 +77,8 @@ public class Geometrias {
 
             GraphFrame graphFrame = new GraphFrame(vertices, edges);
 
-            Dataset<Row> quadrilaterals = findQuads(graphFrame);
-            quadrilaterals.show();
+            Dataset<Row> quads = findQuads(graphFrame);
+            quads.show();
 
 //            Dataset<Row> validQuads = validQuads(graphFrame, quadrilaterals);
 //            validQuads.show();
@@ -98,6 +101,14 @@ public class Geometrias {
     private static Dataset<Row> findQuads(GraphFrame graphFrame) {
         Dataset<Row> quads = graphFrame.find("(A)-[ab]->(B); (B)-[bc]->(C); (C)-[cd]->(D); (D)-[da]->(A)")
                 .filter("A != B AND A != C AND A != D AND B != C AND B != D AND C != D")
+                .filter(row -> {
+                    Row A = row.getAs("A");
+                    Row B = row.getAs("B");
+                    Row C = row.getAs("C");
+                    Row D = row.getAs("D");
+
+                    return !segmentsIntersect(A, B, C, D);
+                })
                 .select(
                         col("A.id").alias("vertexA"),
                         col("B.id").alias("vertexB"),
@@ -107,20 +118,34 @@ public class Geometrias {
 
         return quads.orderBy("vertexA");
     }
-    private static Dataset<Row> validQuads(GraphFrame graphFrame, Dataset<Row> quads) {
-        return quads.filter(row -> {
-            long vertexA = row.getLong(0);
-            long vertexB = row.getLong(1);
-            long vertexC = row.getLong(2);
-            long vertexD = row.getLong(3);
 
-            return graphFrame.edges().filter(
-                    (col("src").equalTo(vertexA).and(col("dst").equalTo(vertexB)))
-                            .or(col("src").equalTo(vertexB).and(col("dst").equalTo(vertexC)))
-                            .or(col("src").equalTo(vertexC).and(col("dst").equalTo(vertexD)))
-                            .or(col("src").equalTo(vertexD).and(col("dst").equalTo(vertexA)))
-            ).count() == 4;
-        });
+    private static boolean segmentsIntersect(Row p1, Row q1, Row p2, Row q2) {
+        //Segment 1
+        int p1x = p1.getAs("x");;
+        int p1y = p1.getAs("y");
+        int q1x = q1.getAs("x");
+        int q1y = q1.getAs("y");
+
+        //Segment 2
+        int p2x = p2.getAs("x");
+        int p2y = p2.getAs("y");
+        int q2x = q2.getAs("x");
+        int q2y = q2.getAs("y");
+
+        int o1 = orientation(p1x, p1y, q1x, q1y, p2x, p2y);
+        int o2 = orientation(p1x, p1y, q1x, q1y, q2x, q2y);
+        int o3 = orientation(p2x, p2y, q2x, q2y, p1x, p1y);
+        int o4 = orientation(p2x, p2y, q2x, q2y, q1x, q1y);
+
+        return o1 != o2 && o3 != o4;
+    }
+
+    public static int orientation(double x1, double y1, double x2, double y2, double x3, double y3) {
+        double valor = (y2 - y1) * (x3 - x2) - (x2 - x1) * (y3 - y2);
+        if (valor == 0) {
+            return 0;
+        }
+        return (valor > 0) ? 1 : -1;
     }
 
     private static String saveResults(Dataset<Row> results, String inputPath) {
